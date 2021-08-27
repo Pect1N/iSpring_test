0a77fd1dae2bb59f4edaf4ef5ce9c8c80af37688
Николай Эпаев <dvd11122@mail.ru>
2021-08-27 21:34:15 +0300

    Added constructor for element (set lower pointer on previous element and set highest element pointer on this element). Also added destructor for element (if element have lower or higher elements {or both} set new pointers)

--- iSpring_test/Model.cpp
+++ iSpring_test/Model.cpp
@@ -2,8 +2,15 @@
 
 Figure::Figure()
 {
+	if (HIGHEST_ELEMENT != 0)
+		lower = HIGHEST_ELEMENT;
+	HIGHEST_ELEMENT = this;
 }
 
 Figure::~Figure()
 {
+	if (higher != 0)
+		higher->lower = lower;
+	if (lower != 0)
+		lower->higher = higher;
 }
