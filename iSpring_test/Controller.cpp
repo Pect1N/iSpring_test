967ac8a3798294fdbd6cf0c276938d28bf9f0d7c
Николай Эпаев <dvd11122@mail.ru>
2021-08-31 17:27:59 +0300

    Added function of creation element

--- iSpring_test/Controller.cpp
+++ iSpring_test/Controller.cpp
@@ -1,1 +1,7 @@
 #include "Controller.h"
+#include "Model.h"
+
+Figure* Element_create(int element_type, Figure* highest)
+{
+	return new Figure(element_type, highest);
+}
--- iSpring_test/Controller.h
+++ iSpring_test/Controller.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include "Model.h"
+
+Figure* Element_create(int, Figure*);
