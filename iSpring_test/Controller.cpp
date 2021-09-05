a545b5b95835711958338cdc0959e164796d4571
Николай Эпаев <dvd11122@mail.ru>
2021-09-05 14:54:35 +0300

    Changed delete function. Added reactions for different situations in list

--- iSpring_test/Controller.cpp
+++ iSpring_test/Controller.cpp
@@ -8,9 +8,36 @@ Figure* Element_create(int element_type, Figure* highest)
 
 Figure* Delete_element(Figure* highest)
 {
-	highest->get_lower()->set_higher(highest->get_higher());
 	Figure* pointer = highest;
-	pointer = pointer->get_lower();
-	delete highest;
-	return pointer;
+	//пробегаем по списку и ищем выделенный элемент
+	if (pointer != NULL)
+	{
+		do
+		{
+			//если такой есть, то удаляем его
+			if (pointer->get_select() == 0)
+			{
+				if (pointer->get_lower() != NULL && pointer->get_higher() != NULL)
+				{
+					pointer->get_lower()->set_higher(pointer->get_higher());
+					pointer->get_higher()->set_lower(pointer->get_lower());
+				}
+				else if (pointer->get_lower() == NULL && pointer->get_higher() == NULL)
+					highest = NULL;
+				else if (pointer->get_lower() == NULL)
+					pointer->get_higher()->set_lower(NULL);
+				else if (pointer->get_higher() == NULL)
+				{
+					pointer->get_lower()->set_higher(NULL);
+					highest = pointer->get_lower();
+				}
+				delete pointer;
+				break;
+			}
+			else
+				pointer = pointer->get_lower();
+		} while (pointer != NULL);
+	}
+	//если нет, то ничего не делаем
+	return highest;
 }
