fe603b5bd774d41172374a72263654d27601970d
Николай Эпаев <dvd11122@mail.ru>
2021-09-12 22:33:35 +0300

    Added function for scaling for different positions.

--- iSpring_test/Controller.cpp
+++ iSpring_test/Controller.cpp
@@ -72,19 +72,101 @@ int Element_click_check(sf::Vector2i position, Figure* highest)
 void Move(sf::Vector2i position, sf::Vector2i startposition, Figure* pointer)
 {
 	sf::Vector2f pos;
+	//проверка на перемещение от размера и скейла
+	//если есть проблемы, то ничего не делать
+	//если всё норм, то применяем это
 	pos.x = pointer->get_position().x + (startposition.x - position.x); // текущая позиция + разность между концом и началом зажима
 	pos.y = pointer->get_position().y + (startposition.y - position.y);
 	pointer->set_position(pos);
 }
 
-void Scale()
-{
-	Minimal_scale_check();
-}
-
 void Minimal_scale_check()
 {
 
+}
+
+void Scale(sf::Vector2i position, sf::Vector2i startposition, Figure* pointer)
+{
+	sf::Vector2f scale;
+	sf::Vector2f pos;
+	//проверка на точку отжатия
+	//если за зоной, то ничего не делать
+	//если всё норм, то выполняем это
+	// лево верх
+	if (startposition.x > pointer->get_position().x && startposition.x < pointer->get_position().x + 20 && 
+		startposition.y > pointer->get_position().y && startposition.y < pointer->get_position().y + 20)
+	{
+		Move(startposition, position, pointer);
+		scale.x = (startposition.x - position.x) / 100.0;
+		scale.x += pointer->get_size().x;
+		scale.y = (startposition.y - position.y) / 100.0;
+		scale.y += pointer->get_size().y;
+	}
+	// лево низ
+	else if (startposition.x > pointer->get_position().x && startposition.x < pointer->get_position().x + 20 && 
+		startposition.y < pointer->get_position().y + pointer->get_size().y * 100 && startposition.y > pointer->get_position().y + pointer->get_size().y * 100 - 20)
+	{
+		pos.x = pointer->get_position().x + (position.x - startposition.x);
+		pos.y = pointer->get_position().y;
+		pointer->set_position(pos);
+		scale.x = (startposition.x - position.x) / 100.0;
+		scale.x += pointer->get_size().x;
+		scale.y = (position.y - startposition.y) / 100.0;
+		scale.y += pointer->get_size().y;
+	}
+	// право верх
+	else if (startposition.x < pointer->get_position().x + pointer->get_size().x * 100 && startposition.x > pointer->get_position().x + pointer->get_size().x * 100 - 20 &&
+		startposition.y > pointer->get_position().y && startposition.y < pointer->get_position().y + 20)
+	{
+		pos.x = pointer->get_position().x;
+		pos.y = pointer->get_position().y + (position.y - startposition.y);
+		pointer->set_position(pos);
+		scale.x = (position.x - startposition.x) / 100.0;
+		scale.x += pointer->get_size().x;
+		scale.y = (startposition.y - position.y) / 100.0;
+		scale.y += pointer->get_size().y;
+	}
+	// право низ
+	else if (startposition.x < pointer->get_position().x + pointer->get_size().x * 100 && startposition.x > pointer->get_position().x + pointer->get_size().x * 100 - 20 &&
+		startposition.y < pointer->get_position().y + pointer->get_size().y * 100 && startposition.y > pointer->get_position().y + pointer->get_size().y * 100 - 20)
+	{
+		scale.x = (position.x - startposition.x) / 100.0;
+		scale.x += pointer->get_size().x;
+		scale.y = (position.y - startposition.y) / 100.0;
+		scale.y += pointer->get_size().y;
+	}
+	else if (startposition.x > pointer->get_position().x && startposition.x < pointer->get_position().x + 20)
+	{
+		pos.x = pointer->get_position().x + (position.x - startposition.x);
+		pos.y = pointer->get_position().y;
+		pointer->set_position(pos);
+		scale.x = (startposition.x - position.x) / 100.0;
+		scale.x += pointer->get_size().x;
+		scale.y = pointer->get_size().y;
+	}
+	else if (startposition.y > pointer->get_position().y && startposition.y < pointer->get_position().y + 20)
+	{
+		pos.x = pointer->get_position().x;
+		pos.y = pointer->get_position().y + (position.y - startposition.y);
+		pointer->set_position(pos);
+		scale.x = pointer->get_size().x;
+		scale.y = (startposition.y - position.y) / 100.0;
+		scale.y += pointer->get_size().y;
+	}
+	else if (startposition.x < pointer->get_position().x + pointer->get_size().x * 100 && startposition.x > pointer->get_position().x + pointer->get_size().x * 100 - 20)
+	{
+		scale.x = (position.x - startposition.x) / 100.0;
+		scale.x += pointer->get_size().x;
+		scale.y = pointer->get_size().y;
+	}
+	else if (startposition.y < pointer->get_position().y + pointer->get_size().y * 100 && startposition.y > pointer->get_position().y + pointer->get_size().y * 100 - 20)
+	{
+		scale.x = pointer->get_size().x;
+		scale.y = (position.y - startposition.y) / 100.0;
+		scale.y += pointer->get_size().y;
+	}
+	pointer->set_size(scale);
+	Minimal_scale_check();
 }
 
 void Long_press_check(sf::Vector2i startposition, sf::Vector2i position, Figure* highest)
@@ -95,7 +177,15 @@ void Long_press_check(sf::Vector2i startposition, sf::Vector2i position, Figure*
 		do
 		{
 			if (pointer->get_select() == 1)
-				Move(startposition, position, pointer);
+			{
+				if (startposition.x > pointer->get_position().x && startposition.x < pointer->get_position().x + 20 ||
+					startposition.y > pointer->get_position().y && startposition.y < pointer->get_position().y + 20 ||
+					startposition.x < pointer->get_position().x + pointer->get_size().x * 100 && startposition.x > pointer->get_position().x + pointer->get_size().x * 100 - 20 ||
+					startposition.y < pointer->get_position().y + pointer->get_size().y * 100 && startposition.y > pointer->get_position().y + pointer->get_size().y * 100 - 20)
+					Scale(position, startposition, pointer);
+				else
+					Move(startposition, position, pointer);
+			}
 			pointer = pointer->get_lower();
 		} while (pointer != NULL);
 	}
--- iSpring_test/Controller.h
+++ iSpring_test/Controller.h
@@ -6,6 +6,6 @@ Figure* Element_create(int, Figure*);
 Figure* Delete_element(Figure*);
 int Element_click_check(sf::Vector2i, Figure*);
 void Long_press_check(sf::Vector2i, sf::Vector2i, Figure*);
-void Scale();
-void Move();
+void Scale(sf::Vector2i, sf::Vector2i, Figure*);
+void Move(sf::Vector2i, sf::Vector2i, Figure*);
 void Minimal_scale_check();
