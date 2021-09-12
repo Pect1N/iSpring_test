d392aef594e93d08ba3266d4fa83f36ff03f8517
Николай Эпаев <dvd11122@mail.ru>
2021-09-12 23:01:36 +0300

    Added new parameters for functions

--- iSpring_test/Controller.h
+++ iSpring_test/Controller.h
@@ -5,7 +5,7 @@
 Figure* Element_create(int, Figure*);
 Figure* Delete_element(Figure*);
 int Element_click_check(sf::Vector2i, Figure*);
-void Long_press_check(sf::Vector2i, sf::Vector2i, Figure*);
-void Scale(sf::Vector2i, sf::Vector2i, Figure*);
-void Move(sf::Vector2i, sf::Vector2i, Figure*);
+void Long_press_check(sf::Vector2i, sf::Vector2i, Figure*, sf::Vector2f, sf::Vector2f);
+void Scale(sf::Vector2i, sf::Vector2i, Figure*, sf::Vector2f, sf::Vector2f);
+void Move(sf::Vector2i, sf::Vector2i, Figure*, sf::Vector2f, sf::Vector2f);
 void Minimal_scale_check();
